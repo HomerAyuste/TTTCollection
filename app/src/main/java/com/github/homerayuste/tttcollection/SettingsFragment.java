c59493d7d08a4459eea9a15cd8fc7cfaf1aa3249
HomerAyuste <lucky.ayuste@gmail.com>
2024-06-28 16:29:22 -0400

    WIP: adding link to github repo when clicking on preference

--- app/src/main/java/com/github/homerayuste/tttcollection/SettingsFragment.java
+++ app/src/main/java/com/github/homerayuste/tttcollection/SettingsFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,6 +57,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        PreferenceManager manager = getPreferenceManager();
+        Preference github = manager.findPreference("github");
+        github.setOnPreferenceClickListener((v)->{return true;});
     }
 
     @Override
