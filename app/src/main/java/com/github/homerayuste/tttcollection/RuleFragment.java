d6de5d399ec6f4be35a4fbaf02b982539c72c5bb
HomerAyuste <lucky.ayuste@gmail.com>
2024-07-25 16:04:44 -0400

    create fragment for 2D 3x3 tic tac toe

--- app/src/main/java/com/github/homerayuste/tttcollection/RuleFragment.java
+++ app/src/main/java/com/github/homerayuste/tttcollection/RuleFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.homerayuste.tttcollection.databinding.FragmentRuleBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,15 +20,18 @@ public class RuleFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private static final String TITLE = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+    private String title = "R ule";
     private String mParam2;
 
     public RuleFragment() {
         // Required empty public constructor
+    }
+    public RuleFragment(String title){
+        this.title = title;
     }
 
     /**
@@ -40,7 +46,7 @@ public class RuleFragment extends Fragment {
     public static RuleFragment newInstance(String param1, String param2) {
         RuleFragment fragment = new RuleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putString(TITLE, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -50,7 +56,7 @@ public class RuleFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            title = getArguments().getString(TITLE);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
@@ -59,6 +65,18 @@ public class RuleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rule, container, false);
+//        View view = inflater.inflate(R.layout.fragment_rule, container, false);
+        //setTitleText(this.title);
+        FragmentRuleBinding fragmentRuleBinding = FragmentRuleBinding.inflate(inflater,container,false);
+        View view = fragmentRuleBinding.getRoot();
+        fragmentRuleBinding.ruleName.setText(this.title);
+        //setTitleText(this.title, view);
+        return view;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
 }
